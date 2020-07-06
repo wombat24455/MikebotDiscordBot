2ac548ec9e53d71ecd840c4c2833e44316d70d89
wombat24455 <53933505+wombat24455@users.noreply.github.com>
2020-07-06 17:10:25 +0100

    Moved embed code
    - moved code for embed outside of command code

--- index.js
+++ index.js
@@ -24,6 +24,195 @@ client.on('message', message=> {
     }
 });
 
+// Embed messages
+
+//help embed
+const helpEmbed = {
+    color: 0x0099ff,
+    title: 'MikeBot Command List',
+    description: 'Here is the "huge" list of my commands lol',
+    thumbnail: {
+        url: 'https://i.vgy.me/HnWKX3.png',
+    },
+    fields: [
+        {
+            name: '>$ping',
+            value: 'I reply with the response time.. you already knew that already didn\'t ya',
+        },
+        {
+            name: '>$invite',
+            value: 'I give you the link to add me to your server',
+        },
+        {
+            name: '>$info [query]',
+            value: 'Available info about me (replace [query] with version or uptime)',
+        },
+        {
+            name: '>$del [amount]',
+            value: 'I bulk delete the amount of messages you tell me to delete',
+        },
+        {
+            name: '>$features',
+            value: 'I reply with a list of stuff that I will be able to do in the future',
+        },
+        {
+            name: '>$website',
+            value: 'I send a link to my website (not 100% functional)',
+        },
+        {
+            name: '>$servercount',
+            value: 'I tell you how many servers I am currently in',
+        },
+        {
+            name: '>$say [args]',
+            value: 'I\'ll say whatever you tell me to say like $say hi',
+        },
+        {
+            name: '>$roadmap',
+            value: 'I send the link to my development roadmap',
+        },
+        {
+            name: 'Nothing here yet buddy',
+            value: 'This part is coming soon:tm:',
+        },
+    ],
+    timestamp: new Date(),
+    footer: {
+        text: 'Commands are prob valid as of',
+    },
+};
+
+// uptime embed
+let totalSeconds = (client.uptime / 1000);
+let days = Math.floor(totalSeconds / 86400);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = Math.floor(totalSeconds % 60);
+let uptime = `${days}d, ${hours}h, ${minutes}m and ${seconds}s`;
+const uptimeEmbed = {
+    color: 0x0099ff,
+    title: 'I have been awake for',
+    fields: [
+        {
+            name: uptime,
+            value: 'I tend to sleep every now and then',
+        },
+    ],
+    timestamp: new Date(),
+    footer: {
+        text: 'Nothin down here buddy'
+    },
+};
+
+// info embed
+const infoEmbed = {
+    color: 0x0099ff,
+    title: 'Bot info',
+    fields: [
+        {
+            name: 'Bot version',
+            value: version,
+            inline: true,
+        },
+        {
+            name: 'Server count',
+            value: `${client.guilds.cache.size} servers`,
+            inline: true,
+        },
+        {
+            name: 'Programming language',
+            value: 'Javascript',
+            inline: true,
+        },
+        {
+            name: 'Bot framework',
+            value: 'Discord.js',
+            inline: true,
+        },
+        {
+            name: 'Bot uptime',
+            value: uptime,
+            inline: false,
+        },
+        {
+            name: 'Bot owner',
+            value: 'AM FREEGe (wombat)#1521',
+        },
+        {
+            name: 'Bot owner ID',
+            value: '546107653718540298',
+        },
+        {
+            name: 'Bot prefix',
+            value: '>$',
+        },
+        {
+            name: 'Invite me',
+            value: '[Click here](https://discord.com/oauth2/authorize?client_id=639421464185143301&scope=bot&permissions=2146958847)',
+            inline: true,
+        },
+        {
+            name: 'My website',
+            value: '[Click here](https://wombat24455.github.io/mikebot.github.io/)',
+            inline: true,
+        },
+        {
+            name: 'My Discord',
+            value: 'Coming soon',
+            inline: true,
+        },
+    ],
+};
+
+// feature embed
+const featureEmbed = {
+    color: 0x0099ff,
+    title: 'This stuff be coming soon',
+    description: 'The stuff listed here might be added soon.',
+    fields: [
+        {
+            name: 'More commands',
+            value: 'I don\'t have many commands rn but more will be added soon.',
+        },
+        {
+            name: 'Better website functionality',
+            value: 'Not much of the website works rn lol (mostly the top part) so I will work on those at some point.',
+        },
+        {
+            name: 'Discord server',
+            value: 'As of right now there is no official/current server for me but one will be created in the near future hopefully.',
+        },
+    ],
+    timestamp: new Date(),
+};
+
+// website embed
+const websiteEmbed = {
+    color: 0x0099ff,
+    title: 'My website:',
+    description: 'My website can be found [here](https://wombat24455.github.io/mikebot.github.io/)',
+    timestamp: new Date(),
+};
+
+// servercount embed
+const servercountEmbed = {
+    color: 0x0099ff,
+    title: 'I am currently in',
+    description: `${client.guilds.cache.size} servers`,
+    timestamp: new Date(),
+};
+
+// roadmap embed
+const roadmapEmbed = {
+    color: 0x0099ff,
+    title: 'MikeBot Development',
+    description: 'My development roadmap can be found [here](https://github.com/wombat24455/MikebotDiscordBot/projects/1)',
+};
+
+// removes the "say" from the command reply
+const sayMessage = args.slice(1).join(' ');
+
 // commands
 client.on('message', message=>{
     if (!message.content.startsWith(prefix)) return;
@@ -32,61 +221,6 @@ client.on('message', message=>{
 
     switch(args[0]){
         case 'help':
-            const helpEmbed = {
-                color: 0x0099ff,
-                title: 'MikeBot Command List',
-                description: 'Here is the "huge" list of my commands lol',
-                thumbnail: {
-                    url: 'https://i.vgy.me/HnWKX3.png',
-                },
-                fields: [
-                    {
-                        name: '>$ping',
-                        value: 'I reply with the response time.. you already knew that already didn\'t ya',
-                    },
-                    {
-                        name: '>$invite',
-                        value: 'I give you the link to add me to your server',
-                    },
-                    {
-                        name: '>$info [query]',
-                        value: 'Available info about me (replace [query] with version or uptime)',
-                    },
-                    {
-                        name: '>$del [amount]',
-                        value: 'I bulk delete the amount of messages you tell me to delete',
-                    },
-                    {
-                        name: '>$features',
-                        value: 'I reply with a list of stuff that I will be able to do in the future',
-                    },
-                    {
-                        name: '>$website',
-                        value: 'I send a link to my website (not 100% functional)',
-                    },
-                    {
-                        name: '>$servercount',
-                        value: 'I tell you how many servers I am currently in',
-                    },
-                    {
-                        name: '>$say [args]',
-                        value: 'I\'ll say whatever you tell me to say like $say hi',
-                    },
-                    {
-                        name: '>$roadmap',
-                        value: 'I send the link to my development roadmap',
-                    },
-                    {
-                        name: 'Nothing here yet buddy',
-                        value: 'This part is coming soon:tm:',
-                    },
-                ],
-                timestamp: new Date(),
-                footer: {
-                    text: 'Commands are prob valid as of',
-                },
-            };
-            
             message.channel.send({ embed: helpEmbed });
             break;
         case 'ping':
@@ -102,94 +236,8 @@ client.on('message', message=>{
             if(args[1] === 'version'){
                 message.channel.send('Bot version: ' + version);
             }else if (args[1] === 'uptime'){
-                let totalSeconds = (client.uptime / 1000);
-                let days = Math.floor(totalSeconds / 86400);
-                let hours = Math.floor(totalSeconds / 3600);
-                totalSeconds %= 3600;
-                let minutes = Math.floor(totalSeconds / 60);
-                let seconds = Math.floor(totalSeconds % 60);
-                let uptime = `${days}d, ${hours}h, ${minutes}m and ${seconds}s`;
-                const uptimeEmbed = {
-                    color: 0x0099ff,
-                    title: 'I have been awake for',
-                    fields: [
-                        {
-                            name: uptime,
-                            value: 'I tend to sleep every now and then',
-                        },
-                    ],
-                    timestamp: new Date(),
-                    footer: {
-                        text: 'Nothin down here buddy'
-                    },
-                };
                 message.channel.send({ embed: uptimeEmbed });
             }else if (args[1] === 'all'){
-                let totalSeconds = (client.uptime / 1000);
-                let days = Math.floor(totalSeconds / 86400);
-                let hours = Math.floor(totalSeconds / 3600);
-                totalSeconds %= 3600;
-                let minutes = Math.floor(totalSeconds / 60);
-                let seconds = Math.floor(totalSeconds % 60);
-                let uptime = `${days}d, ${hours}h, ${minutes}m and ${seconds}s`;
-                const infoEmbed = {
-                    color: 0x0099ff,
-                    title: 'Bot info',
-                    fields: [
-                        {
-                            name: 'Bot version',
-                            value: version,
-                            inline: true,
-                        },
-                        {
-                            name: 'Server count',
-                            value: `${client.guilds.cache.size} servers`,
-                            inline: true,
-                        },
-                        {
-                            name: 'Programming language',
-                            value: 'Javascript',
-                            inline: true,
-                        },
-                        {
-                            name: 'Bot framework',
-                            value: 'Discord.js',
-                            inline: true,
-                        },
-                        {
-                            name: 'Bot uptime',
-                            value: uptime,
-                            inline: false,
-                        },
-                        {
-                            name: 'Bot owner',
-                            value: 'AM FREEGe (wombat)#1521',
-                        },
-                        {
-                            name: 'Bot owner ID',
-                            value: '546107653718540298',
-                        },
-                        {
-                            name: 'Bot prefix',
-                            value: '>$',
-                        },
-                        {
-                            name: 'Invite me',
-                            value: '[Click here](https://discord.com/oauth2/authorize?client_id=639421464185143301&scope=bot&permissions=2146958847)',
-                            inline: true,
-                        },
-                        {
-                            name: 'My website',
-                            value: '[Click here](https://wombat24455.github.io/mikebot.github.io/)',
-                            inline: true,
-                        },
-                        {
-                            name: 'My Discord',
-                            value: 'Coming soon',
-                            inline: true,
-                        },
-                    ],
-                };
                 message.channel.send({ embed: infoEmbed });
             }else{
                 message.channel.send('Bro do ``>$help`` so you know how to use the damn command.');
@@ -207,57 +255,19 @@ client.on('message', message=>{
                 }
             break;
             case 'features':
-                const featureEmbed = {
-                    color: 0x0099ff,
-                    title: 'This stuff be coming soon',
-                    description: 'The stuff listed here might be added soon.',
-                    fields: [
-                        {
-                            name: 'More commands',
-                            value: 'I don\'t have many commands rn but more will be added soon.',
-                        },
-                        {
-                            name: 'Better website functionality',
-                            value: 'Not much of the website works rn lol (mostly the top part) so I will work on those at some point.',
-                        },
-                        {
-                            name: 'Discord server',
-                            value: 'As of right now there is no official/current server for me but one will be created in the near future hopefully.',
-                        },
-                    ],
-                    timestamp: new Date(),
-                };
                 message.channel.send({ embed: featureEmbed });
             break;
             case 'website':
-                const websiteEmbed = {
-                    color: 0x0099ff,
-                    title: 'My website:',
-                    description: 'My website can be found [here](https://wombat24455.github.io/mikebot.github.io/)',
-                    timestamp: new Date(),
-                };
                 message.channel.send({ embed: websiteEmbed });
             break;
             case 'servercount':
-                const servercountEmbed = {
-                    color: 0x0099ff,
-                    title: 'I am currently in',
-                    description: `${client.guilds.cache.size} servers`,
-                    timestamp: new Date(),
-                };
                 message.channel.send({ embed: servercountEmbed });
             break;
             case 'say':
-                const sayMessage = args.slice(1).join(' ');
                 message.delete();
                 message.channel.send(sayMessage);
             break;
             case 'roadmap':
-                const roadmapEmbed = {
-                    color: 0x0099ff,
-                    title: 'MikeBot Development',
-                    description: 'My development roadmap can be found [here](https://github.com/wombat24455/MikebotDiscordBot/projects/1)',
-                };
                 message.channel.send({ embed: roadmapEmbed })
             break;
     }
