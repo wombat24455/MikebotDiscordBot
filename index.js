59e1d126046ba486844b28670f55bf96863b614e
wombat24455 <53933505+wombat24455@users.noreply.github.com>
2020-07-10 23:38:10 +0100

    Adjusting >$suggest command

--- index.js
+++ index.js
@@ -303,7 +303,7 @@ client.on('message', message=>{
             break;
             case 'suggest':
                 suggestion = args.slice(1).join(' ');
-                client.channels.cache.get('731267852564430882').send(suggestion + " - Suggested by " + `<@message.author.id>`)
+                client.channels.cache.get('731267852564430882').send(suggestion + " - Suggested by " + message.author.id)
             break;
     }
 })
