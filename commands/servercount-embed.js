5f869b1753c85700378976c94da2f66bc8f33997
wombat24455 <wombathodge@gmail.com>
2020-10-03 23:54:36 +0100

    ffs

--- commands/servercount-embed.js
+++ commands/servercount-embed.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'servercount',
 	description: 'Displays the number of servers the bot is in',
-	execute(message, client) {
+	execute(message, args, client) {
     const servercountEmbed = {
         color: 0x0099ff,
         title: 'I am currently in',
