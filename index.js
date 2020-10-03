0012313cfdbb0012144edd794226776b41ac8695
wombat24455 <wombathodge@gmail.com>
2020-10-03 23:22:08 +0100

    bruh wtf is going on

--- index.js
+++ index.js
@@ -26,45 +26,46 @@ var version = '1.17.15';
 
 // outputs in console when bot code is run
 client.on('ready', () => {
-	user_count = client.guilds.cache.map((g) => g.memberCount).reduce((a, b) => a + b);
-	guildNames = client.guilds.cache.map(g => g.name).join("\n");
-	console.log(`Bot logged in as ${client.user.tag}, watching ${client.guilds.cache.size} servers, and serving over ${user_count} users`);
-	console.log(`I am currently in:\n${guildNames}`);
+    user_count = client.guilds.cache.map((g) => g.memberCount).reduce((a, b) => a + b);
+    guildNames = client.guilds.cache.map(g => g.name).join("\n");
+    console.log(`Bot logged in as ${client.user.tag}, watching ${client.guilds.cache.size} servers, and serving over ${user_count} users`);
+    console.log(`I am currently in:\n${guildNames}`);
 
-	client.user.setPresence({
-		status: 'online',
-		activity: {
-			name: 'https://cwavs.xyz',
-			type: 'WATCHING',
-		}
-	})
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: 'my developers fail',
+            type: 'WATCHING',
+        }
+    })
 })
 
 // Replies to mention of bot
 client.on('message', message => {
-	if (message.mentions.has(client.user)) {
-			message.channel.send(`my prefix is >$ buddy.`);
-	}
+    if (message.mentions.has(client.user)) {
+        message.channel.send(`my prefix is >$ buddy.`);
+    }
 });
 
 // Prevents usage of @everyone and @here in >$say or >$suggestion messages
 const nonoWord = "@everyone";
 const nonoWord2 = "@here";
 
-// listens for commands
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+// commands
+client.on('message', async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
 	if (!client.commands.has(command)) return;
 
 	try {
-		client.commands.get(command).execute(message, args);
+        client.commands.get(command).execute(message, args);
+
 	} catch (error) {
 		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+        message.reply('there was an error trying to execute that command!');
+    }
 });
 client.login(token);
