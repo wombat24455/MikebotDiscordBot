e961224acbc24d0acf519b7d873bcbd2111597fb
wombat24455 <wombathodge@gmail.com>
2020-10-12 22:12:41 +0100

    dunno wtf is wrong

--- commands/givmeme.js
+++ commands/givmeme.js
@@ -20,16 +20,16 @@ module.exports = {
     let subreddit = reddit[Math.floor(Math.random() * reddit.length)];
 
     randomPuppy(subreddit).then(async url => {
-        const memeEmbed = {
-            color: 0x0099ff,
-            title: 'this was stolen from r/' + subreddit,
-            description: `[Open in browser](${url})`,
-            url: `https://www.reddit.com/r/${subreddit}`,
-            image: {
-                url: url,
-            },
-        };
-    });
-		message.channel.send({ embed: memeEmbed });
-	},
+			const memeEmbed = {
+					color: 0x0099ff,
+					title: 'this was stolen from r/' + subreddit,
+					description: `[Open in browser](${url})`,
+					url: `https://www.reddit.com/r/${subreddit}`,
+					image: {
+							url: url,
+					},
+			};
+			message.channel.send({ embed: memeEmbed });
+		});
+		},
 };
