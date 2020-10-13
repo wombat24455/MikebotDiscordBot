68fa3f131fabb5442310b1649b8c311353e9fe72
Cwavs <kyle.meehan03@gmail.com>
2020-10-13 11:20:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on('ready', () => {
 // Replies to mention of bot
 client.on('message', message => {
   if (message.content.includes(client.user.id)) {
-    message.channel.send(`my prefix is ${prefix} buddy.`);
+    message.channel.send(`my prefix is ${prefix} buddy. <@639421464185143301>`);
   }
 });
 
