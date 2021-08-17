732f5665f32c5c8491e014cff6f4f6ab8827544e
wombat24455 <wombathodge@gmail.com>
2021-08-17 03:09:24 +0100

    reverted ping command

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'yeah idk how this works',
 	execute(message, client) {
     message.channel.send("Pinging...").then(m => {
-        m.edit(`Roundtrip latency: **${m.createdTimestamp - message.createdTimestamp}ms**\nWebsocket heartbeat: ${client.ws.ping}**ms.`);
+        m.edit(`Roundtrip latency: **${m.createdTimestamp - message.createdTimestamp}**ms`/* \nWebsocket heartbeat: ${client.ws.ping}**ms .*/);
     });
 	},
 };
