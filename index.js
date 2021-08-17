2671e021e99c2a25050f75bb103fa72bb1bf1f9a
wombat24455 <wombathodge@gmail.com>
2021-08-17 01:06:02 +0100

    hopefully this shit is fixed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const intents = new Discord.Intents(32767);
-const client = new Discord.Client({ intents });
+const client = new Discord.Client({ intents: 14023 });
 
 client.commands = new Discord.Collection();
 
