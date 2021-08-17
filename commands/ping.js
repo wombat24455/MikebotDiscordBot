c48ffd672dee7181a18dd2a90b57ff3fc204627e
wombat24455 <wombathodge@gmail.com>
2021-08-17 02:37:34 +0100

    changed ping command (fixing bot presence)

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,8 @@ module.exports = {
 	name: 'ping',
 	description: 'yeah idk how this works',
 	execute(message) {
-    message.channel.send("Pinging...").then(m => {
-        var ping = m.createdTimestamp - message.createdTimestamp;
-        m.edit(`Roundtrip latency: **${m.createdTimestamp - message.createdTimestamp}ms**\nWebsocket heartbeat: ${client.ws.ping}ms.`);
+    message.channel.send("Pinging...").then(msg => {
+        m.edit(`Roundtrip latency: **${msg.createdTimestamp - message.createdTimestamp}**ms\nWebsocket heartbeat: **${client.ws.ping}**ms.`); //not sure if client.ws.ping will work
     });
 	},
 };
