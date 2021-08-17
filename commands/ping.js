d21460ff62cbc6759d347f2564fbedef0025a755
wombat24455 <wombathodge@gmail.com>
2021-08-17 02:46:53 +0100

    dumb fix XD

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'yeah idk how this works',
 	execute(message) {
     message.channel.send("Pinging...").then(msg => {
-        m.edit(`Roundtrip latency: **${msg.createdTimestamp - message.createdTimestamp}**ms\nWebsocket heartbeat: **${client.ws.ping}**ms.`); //not sure if client.ws.ping will work
+        msg.edit(`Roundtrip latency: **${msg.createdTimestamp - message.createdTimestamp}**ms\nWebsocket heartbeat: **${client.ws.ping}**ms.`); //not sure if client.ws.ping will work
     });
 	},
 };
