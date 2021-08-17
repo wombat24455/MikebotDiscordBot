24fc78be21f18b122da3a3fe1004ccc333908415
wombat24455 <53933505+wombat24455@users.noreply.github.com>
2021-08-17 17:53:23 +0100

    fix dumb mistake XD

--- commands/setpfp.js
+++ commands/setpfp.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'setpfp',
   description: 'changes profile picture of bot (bot owner only)',
-  execute(message, client) {
+  execute(message/*, client*/) {
     switch (message.author.id) {
       case '546107653718540298':
         newpfp = message.attachments.first().url
