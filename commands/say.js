9b4aafd736f5f444102192da106ebccc0c18b27f
wombat24455 <wombathodge@gmail.com>
2021-08-17 17:05:53 +0100

    testing this stuff

--- commands/say.js
+++ commands/say.js
@@ -7,12 +7,29 @@ module.exports = {
   description: 'Get MikeBot to say what you want.',
   execute(message, args) {
     const sayMessage = args.join(' ');
+		/*
     if (message.content.includes(everyonePing || herePing)) {
       message.delete();
       message.reply("I am not saying that >:(");
     } else {
       message.delete();
       message.channel.send(sayMessage);
+    }
+		*/
+
+    switch (message.content.includes()) {
+      case everyonePing:
+        message.delete();
+        message.reply("I am not saying that >:(");
+				break;
+			case herePing:
+				message.delete();
+				message.reply("I am not saying that >:(");
+				break;
+			default:
+				message.delete();
+				message.channel.send(sayMessage);
+				break;
     }
   },
 };
