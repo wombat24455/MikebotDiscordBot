3bb2b7cb2e7dd50e35383777f635821b50891469
wombat24455 <wombathodge@gmail.com>
2021-08-22 02:47:45 +0100

    formatting (still broken)

--- commands/del.js
+++ commands/del.js
@@ -4,17 +4,17 @@ module.exports = {
 	execute(message, args) {
 		if (message.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)) {
 			if (!args[0]) {
-					message.reply('You need to tell me the number of messages you want to delete you numpty.');
-					return;
+				message.reply('You need to tell me the number of messages you want to delete you numpty.');
+				return;
 			}
 			var newamount = Number(args[0]) + Number(1);
 			message.channel.bulkDelete(newamount);
 			message.channel.send('Yeeted ' + args[0] + ' messages into the void.').then(message => {
 					message.delete({ timeout: 3000 });
-			})
-					.catch(console.error);
-	} else {
+				})
+				.catch(console.error);
+		} else {
 			message.reply('How about no');
-	}
+		}
 	},
 };
