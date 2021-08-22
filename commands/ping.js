32fe592f6973ccb15304211019586ace1ee5c201
wombat24455 <wombathodge@gmail.com>
2021-08-22 02:22:01 +0100

    debug 2: the bullshit continues

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,9 @@ module.exports = {
 	name: 'ping',
 	description: 'yeah idk how this works',
 	execute(message, client) {
-		console.log(client.token);
-    message.channel.send("Pinging...").then(m => {
-        m.edit(`Roundtrip latency: \`${m.createdTimestamp - message.createdTimestamp}ms\`\nAPI Latency: \`${Math.round(client.ws.ping)}ms\``);
-    });
+		message.channel.send("Pinging...").then(m => {
+			console.log(client.ws);
+			m.edit(`Roundtrip latency: \`${m.createdTimestamp - message.createdTimestamp}ms\`\nAPI Latency: \`${Math.round(client.ws.ping)}ms\``);
+		});
 	},
 };
