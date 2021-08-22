396aa47c0a378c4e84b20c8e9a0947ff227ec7e3
wombat24455 <wombathodge@gmail.com>
2021-08-22 01:27:51 +0100

    go away bot latency

--- commands/ping.js
+++ commands/ping.js
@@ -5,7 +5,7 @@ module.exports = {
 	description: 'yeah idk how this works',
 	execute(message, client) {
     message.channel.send("Pinging...").then(m => {
-        m.edit(`Roundtrip latency: \`${m.createdTimestamp - message.createdTimestamp}ms\`\nBot latency: ${Math.round(client.ping)}\nAPI Latency: \`${Math.round(client.ws.ping)}ms\``);
+        m.edit(`Roundtrip latency: \`${m.createdTimestamp - message.createdTimestamp}ms\`\nAPI Latency: \`${Math.round(client.ws.ping)}ms\``);
     });
 	},
 };
