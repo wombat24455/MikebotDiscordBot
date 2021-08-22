632ac30a251b9546055d323c03c41d071634065e
wombat24455 <wombathodge@gmail.com>
2021-08-22 01:24:04 +0100

    probably wont work lmao

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,11 @@
+const { Client } = require("discord.js");
+
 module.exports = {
 	name: 'ping',
 	description: 'yeah idk how this works',
-	execute(message) {
+	execute(message, client) {
     message.channel.send("Pinging...").then(m => {
-        m.edit(`Roundtrip latency: \`${m.createdTimestamp - message.createdTimestamp}\` ms`);
+        m.edit(`Roundtrip latency: \`${m.createdTimestamp - message.createdTimestamp}ms\`\nBot latency: ${Math.round(client.ping)}\nAPI Latency: \`${Math.round(client.ws.ping)}ms\``);
     });
 	},
 };
