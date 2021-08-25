f17d3a24cbfdee348cb7ae8ae59f1df745dd3c67
wombat24455 <wombathodge@gmail.com>
2021-08-25 19:18:47 +0100

    updated to ES6 module #2

--- commands/say.js
+++ commands/say.js
@@ -2,20 +2,18 @@
 var everyonePing = '@everyone';
 var herePing = '@here';
 
-module.exports = {
-  name: 'say',
-  description: 'Get MikeBot to say what you want.',
-  execute(message, args) {
-    const sayMessage = args.join(' ');
-    if (message.content.includes(everyonePing)) {
-      message.delete();
-      message.reply("I am not saying that >:(");
-    } else if (message.content.includes(herePing)) {
-      message.delete();
-      message.reply("I am not saying that >:(");
-    } else {
-      message.delete();
-      message.channel.send(sayMessage);
-    }
-  },
-};
+export const name = 'say';
+export const description = 'Get MikeBot to say what you want.';
+export function execute(message, args) {
+  const sayMessage = args.join(' ');
+  if (message.content.includes(everyonePing)) {
+    message.delete();
+    message.reply("I am not saying that >:(");
+  } else if (message.content.includes(herePing)) {
+    message.delete();
+    message.reply("I am not saying that >:(");
+  } else {
+    message.delete();
+    message.channel.send(sayMessage);
+  }
+}
