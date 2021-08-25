494c47d497869531d82b2508986c407a92709908
wombat24455 <wombathodge@gmail.com>
2021-08-25 19:45:17 +0100

    fix #2

--- commands/actinsusngl.js
+++ commands/actinsusngl.js
@@ -1,19 +1,18 @@
-import randomPuppy from 'random-puppy';
-
 export const name = 'actinsusngl';
 export const description = 'gets memes from r/AmongUs';
 export function execute(message) {
-    let amongUsSubreddit = "AmongUs";
-    randomPuppy(amongUsSubreddit).then(async (url) => {
-        const amongUsmemeEmbed = {
-            color: 0x0099ff,
-            title: 'this was stolen from r/' + amongUsSubreddit,
-            description: `[Open in browser](${url})`,
-            url: `https://www.reddit.com/r/${amongUsSubreddit}`,
-            image: {
-                url: url,
-            },
-        };
-        message.channel.send({ embeds: [amongUsmemeEmbed] });
-    });
+  import randomPuppy from 'random-puppy';
+  let amongUsSubreddit = "AmongUs";
+  randomPuppy(amongUsSubreddit).then(async (url) => {
+    const amongUsmemeEmbed = {
+      color: 0x0099ff,
+      title: 'this was stolen from r/' + amongUsSubreddit,
+      description: `[Open in browser](${url})`,
+      url: `https://www.reddit.com/r/${amongUsSubreddit}`,
+      image: {
+        url: url,
+      },
+    };
+    message.channel.send({eds: [amongUsmemeEmbed]});
+  });
 }
