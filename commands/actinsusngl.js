2a39cf9065aa1bf79838db211d23bea223c06008
wombat24455 <wombathodge@gmail.com>
2021-08-25 20:52:28 +0100

    SUS EMBEDS

--- commands/actinsusngl.js
+++ commands/actinsusngl.js
@@ -13,6 +13,6 @@ export function execute(message) {
         url: url,
       },
     };
-    message.channel.send({eds: [amongUsmemeEmbed]});
+    message.channel.send({embeds: [amongUsmemeEmbed]});
   });
 }
