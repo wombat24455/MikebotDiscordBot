a1c1879105206be6ae8022e6d782aa4f4e97d44a
wombat24455 <wombathodge@gmail.com>
2021-08-25 19:50:46 +0100

    bryh

--- commands/suggest.js
+++ commands/suggest.js
@@ -7,8 +7,8 @@ export function execute(message, args, client) {
 
     const everyone = _everyonePing;
     const here = _herePing;
+    const suggestion = args.join(' ');
 
-    suggestion = args.join(' ');
     if (message.content.includes(everyone)) {
         message.reply("Nice try buckaroo you can't get me to mention everyone");
     } else if (message.content.includes(here)) {
