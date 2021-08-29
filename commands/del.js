40ea26439f461563a7abdb53f285e8e9eef219e9
wombat24455 <wombathodge@gmail.com>
2021-08-29 04:14:09 +0100

    is it fixed? :eyes:

--- commands/del.js
+++ commands/del.js
@@ -1,15 +1,15 @@
-module.exports = { 
+module.exports = {
 	name: 'del',
 	description: 'Deletes the amount of messages specified (limit of 100)',
-	execute(message, args) { 
-		if (message.member.permissions.has(Permissions.MANAGE_MESSAGES)) {
+	execute(message, args) {
+		if (message.author.permissions.has(Permissions.MANAGE_MESSAGES)) {
 			if (!args[0]) {
 				message.reply('You need to tell me the number of messages you want to delete you numpty.');
 				return;
 			}
 			var newamount = Number(args[0]) + Number(1);
 			message.channel.bulkDelete(newamount);
-			message.channel.send('Yeeted ' + args[0] + ' messages into the void.').then(message => {
+			message.channel.send(`Yeeted ${args[0]} messages into the void.`).then(message => {
 					message.delete({ timeout: 3000 });
 				})
 				.catch(console.error);
