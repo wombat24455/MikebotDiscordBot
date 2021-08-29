8e6e6431c88e40199ab2e56a19003ce9b5fa146d
techman471 <techman471@outlook.com>
2021-08-28 22:02:46 -0500

    Do not question it

--- commands/gh.js
+++ commands/gh.js
@@ -1,20 +1,21 @@
-module.exports = { 
-	name: 'gh',
-    alias: 'repo',
-	description: 'I reply with the link to my github',
-	execute(message) {
-        const ghEmbed = {
-            color: 0x0099ff,
-            title: 'GitHub Repository',
-            description: 'Helo :)',
-            fields: [
-                {
-                    name: 'This is the link to my github:',
-                    value: 'https://github.com/wombat24455/MikebotDiscordBot',
-                },
-            ],
-            timestamp: new Date(),
-        };
-        message.channel.send({ embeds: [ghEmbed] });
-    }
+if (message == `gh` || `github`) {
+  module.exports = {
+    name: message,
+    description: "I reply with the link to my github",
+    execute(message) {
+      const ghEmbed = {
+        color: 0x0099ff,
+        title: "GitHub Repository",
+        description: "Helo :)",
+        fields: [
+          {
+            name: "This is the link to my github:",
+            value: "https://github.com/wombat24455/MikebotDiscordBot",
+          },
+        ],
+        timestamp: new Date(),
+      };
+      message.channel.send({ embeds: [ghEmbed] });
+    },
+  };
 }
