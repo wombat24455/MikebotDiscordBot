0a771a95a76f0c084190991fec549ad1d46f9a81
Ci Astra <68437385+cixastra@users.noreply.github.com>
2021-09-10 16:28:01 -0500

    maybe a servercount fix? I might also have broken it

--- commands/servercount-embed.js
+++ commands/servercount-embed.js
@@ -5,7 +5,7 @@ module.exports = {
         const servercountEmbed = {
             color: 0x0099ff,
             title: 'I am currently in',
-            description: `${client.guilds.cache.size} servers`,
+            description: `${client.guilds} servers`,
             timestamp: new Date(),
         };
         message.channel.send({ embeds: [servercountEmbed] });
