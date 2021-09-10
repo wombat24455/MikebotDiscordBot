7f674192fe223883dd9d30d1d542fd82be8c5e18
Ci Astra <68437385+cixastra@users.noreply.github.com>
2021-09-10 16:40:40 -0500

    servercount-embed.js fix

--- commands/servercount-embed.js
+++ commands/servercount-embed.js
@@ -1,13 +1,13 @@
 module.exports = {
 	name: 'servercount',
 	description: 'Displays the number of servers the bot is in',
-	execute(message, client) {
+    execute: function (message, client) {
         const servercountEmbed = {
             color: 0x0099ff,
             title: 'I am currently in',
-            description: `${client.guilds} servers`,
+            description: client.guilds.cache.size + ` servers`,
             timestamp: new Date(),
         };
-        message.channel.send({ embeds: [servercountEmbed] });
+        message.channel.send({embeds: [servercountEmbed]});
     }
 }
