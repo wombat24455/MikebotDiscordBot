1790c5e8475cfc0a922a16b50963f442eb156dea
wombat24455 <wombathodge@gmail.com>
2021-09-11 21:11:16 +0100

    bruh moment

--- commands/ping.js
+++ commands/ping.js
@@ -2,9 +2,9 @@ module.exports = {
 	name: 'ping',
 	description: 'yeah idk how this works',
 	execute(message, client) {
-		console.log(client);
+		//console.log(client);
 		message.reply("Pinging...").then(m => {
-			m.edit(`Roundtrip latency: \`${m.createdTimestamp - message.createdTimestamp}ms`); // \nAPI Latency: \`${Math.round(client.ws.ping)}ms\``);
+			m.edit(`Roundtrip latency: \`${m.createdTimestamp - message.createdTimestamp}ms\``); // \nAPI Latency: \`${Math.round(client.ws.ping)}ms\``);
 		});
 	}
 }
